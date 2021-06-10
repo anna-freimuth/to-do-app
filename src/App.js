167d19706c844788d97308b2db4b80f2b764a6c4
Anna Freimuth <a.freimuth90@gmail.com>
2021-06-10 08:49:56 +0200

    app before components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <section id="form">
+                <input type="text" name="" id=""/>
+                <button>Add</button>
+            </section>
+            <section id="list">
+                <ul>
+                    <li>To-Do Item<span>Actions(complete,delete)</span></li>
+                </ul>
+            </section>
+        </div>
+    );
 }
 
 export default App;
